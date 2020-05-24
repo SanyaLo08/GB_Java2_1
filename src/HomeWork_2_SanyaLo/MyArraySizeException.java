38e143f6f4ae06db663a9e3361cb82c69fcde5aa
Alexander <9265501657@mail.ru>
2020-05-25 01:23:53 +0300

    HomeWork2 Не совсем доделал. (ошибки)

---
+++ src/HomeWork_2_SanyaLo/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package HomeWork_2_SanyaLo;
+
+public class MyArraySizeException {
+    public Object String () {
+        return "Массив не равен 4 Х 4 !"; 
+    }
+
+    public void printStackTrace() {
+    }
+}
